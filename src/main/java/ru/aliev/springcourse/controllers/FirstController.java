1a8f57e4938297166ee281b1a83e9a9450fb3f00
Djanik Aliyev <djanik_aliyev@mail.ru>
2024-04-05 21:05:16 +0300

    Use Model to hand over data from controller to view

--- src/main/java/ru/aliev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/aliev/springcourse/controllers/FirstController.java
@@ -1,6 +1,8 @@
 package ru.aliev.springcourse.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,14 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/first")
 public class FirstController {
-
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false)
                             String name,
                             @RequestParam(value = "surname", required = false)
-                            String surname) {
-
-        System.out.println("Hello, " + name + " " + surname);
+                            String surname,
+                            Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("surname", surname);
 
         return "first/hello";
     }
